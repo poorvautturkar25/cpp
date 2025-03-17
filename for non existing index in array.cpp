c7eb4729d74246579407a7098f404e4dd9248182
poorvautturkar25 <poorvautturkar05@gmail.com>
2025-03-17 11:18:52 +0530

    Add files via upload

---
+++ for non existing index in array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stdexcept>
+using namespace std;
+int main()
+{
+	int arr[5]={1,2,3,4,5},index;
+	cout<<"Enter the index:";
+	cin>>index;
+	
+	try
+	{
+		if(index<0 || index>4) 
+		throw out_of_range("Invalid Index");
+		cout<<"Element at"<<index<<"is"<<arr[index];
+	}
+	
+	catch(out_of_range &str)
+	{
+		cout<<"Exception:"<<str.what();
+	}
+	return 0; 
+}
